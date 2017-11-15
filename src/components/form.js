02270d7fc4c1cc889695955229f5ede657f8a47a
MoseyM <mmoseyee@gmail.com>
2017-11-15 13:39:47 -0600

    got the form reset working

--- src/components/form.js
+++ src/components/form.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import PayoffBlock from './payoffBlock';
+
 
 class Form extends React.Component
 
@@ -9,23 +11,24 @@ class Form extends React.Component
 		this.state = {
 			fields: {
 				principal: {
-					value: null,
+					value: '',
 					label: "Principal",
 					type: "currency"
 				},
 				interest: {
-					value: null,
+					value: '',
 					label: "Interest Rate"
 				},
 				payment: {
-					value: null,
-					label: "Payment per Period"
+					value: '',
+					label: "Payment per Period",
+					isLast: true
 				}
 			},
 			iterator:0
 		};
 		this.change = this.change.bind(this);
-		this.onkeySubmit = this.onkeySubmit.bind(this);
+		this.onKeySubmit = this.onKeySubmit.bind(this);
 	}
 
 	componentWillMount() {
@@ -35,15 +38,20 @@ class Form extends React.Component
 	}
 
 	render() {
-		var b = "fdfsdaf"
+		let result;
+		if(this.state.fields[this.state.labels[this.state.iterator]].isLast) {
+            result = <PayoffBlock paymentDetails={ this.state.fields }/>;
+		}
 		return (
-			<form onSubmit={this.onkeySubmit}>
-				<h3 id="inputHead"></h3>
-				<input type="text" id="{this.state.labels[this.state.iterator]}" onChange={this.change}
-				 placeholder={this.state.fields[this.state.labels[this.state.iterator]].label} />
-				<p id="error"></p>
-			</form>
-
+			<div>
+				<h3 id="title">{this.state.fields[this.state.labels[this.state.iterator]].label}</h3>
+				<form id="finForm" onSubmit={this.onKeySubmit}>
+					<input type="text" id={this.state.labels[this.state.iterator]} onChange={this.change}
+						   placeholder={this.state.fields[this.state.labels[this.state.iterator]].label}
+						   defaultValue={this.state.fields[this.state.labels[this.state.iterator]].value} />
+					<p id="error"></p>
+				</form>
+			</div>
 		);
 	}
 
@@ -56,16 +64,28 @@ class Form extends React.Component
 		}
 	}
 
-	onkeySubmit(e) {
-		let j = document.getElementById("error");
-		e.preventDefault();
-		console.log("Submit!")
-		if(isNaN(e.target.value) ) {
-			j.innerHTML = "This must be a number";
-		} else {
-			console.log(e.target.id)
+	onKeySubmit(e) {
+        e.preventDefault();
+        let j = document.getElementById("error");
+		let currentInput = document.getElementById(this.state.labels[this.state.iterator]);
+		if(isNaN(currentInput.value) ) {
+            j.innerHTML = "This must be a number";
+            return false;
+        }
+        this.state.fields[this.state.labels[this.state.iterator]].value = currentInput.value;
+		let count = this.state.iterator + 1;
+        if(this.state.fields[this.state.labels[this.state.iterator]].isLast) {
+        	this.setState({
+                iterator: 0
+            })
+        } else {
+            this.setState({
+                iterator: count
+            })
 		}
-	}			
+		document.getElementById("finForm").reset();
+	}
+
 }
 
 export default Form
